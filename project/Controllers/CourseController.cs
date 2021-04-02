fa19f76e82ca8be2c3aaefc88aa489b38429682c
Nguyễn Thành Huy <huy0978178327@gmail.com>
2021-04-02 23:53:35 +0700

    CreateCourse

--- project/Controllers/CourseController.cs
+++ project/Controllers/CourseController.cs
@@ -14,40 +14,15 @@ namespace project.Controllers
     public class CourseController : Controller
     {
         private EducationDBContext db = new EducationDBContext();
-        // GET: Course
-        //public bool checkAuthentication()
-        //{
-        //    string absolutepath = HttpContext.Request.Url.AbsolutePath;
-        //    //check authorize
-        //    User user = (User)Session["account"];
-        //    if (user == null)
-        //    {
-        //        return false;
-        //    }
-        //    foreach (var item in user.features)
-        //    {
-        //        if (item.Url.Equals(absolutepath))
-        //        {
-        //            //if equal
-        //            return true;
-        //        }
-        //    }
-        //    return false;
-        //}
+
 
         public ActionResult Course()
         {
-            //if (checkAuthentication())
-            //{
-                var course = (from s in db.Courses select s).ToList();
-                ViewBag.courses = course;
-                return View();
-            //}
-            //else
-            //{
-            //    ViewBag.MyMessage = "You need to login as a Teacher account or Student account to have permission to acces this page";
-            //    return View("Error");
-            //}
+
+            var course = (from s in db.Courses select s).ToList();
+            ViewBag.courses = course;
+            return View();
         }
+
     }
 }
