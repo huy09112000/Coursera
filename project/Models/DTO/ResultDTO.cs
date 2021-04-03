fc3a3a4ec3ce5fc0d3dc3459481b9ca084d840ec
vikto132 <60517065+vikto132@users.noreply.github.com>
2021-04-03 10:57:43 +0700

    add request on class angain

---
+++ project/Models/DTO/ResultDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace project.Models.DTO
+{
+    public class ResultDTO
+    {
+        public int QuesId { get; set; }
+        public int AnsChoosed { get; set; }
+        public int CorrectAns { get; set; }
+        public int totalPoint { get; set; }
+        public int QuizzId { get; set; }
+    }
+}
