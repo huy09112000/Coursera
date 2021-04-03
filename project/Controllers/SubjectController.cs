6231d15feedf3b940c4080649c00e5c977398530
Nguyễn Thành Huy <huy0978178327@gmail.com>
2021-04-03 10:43:47 +0700

    ratingStar

--- project/Controllers/SubjectController.cs
+++ project/Controllers/SubjectController.cs
@@ -14,6 +14,8 @@ namespace project.Controllers
     {
         private EducationDBContext db = new EducationDBContext();
         // GET: Subject
+
+        //load va get value rate ve
         public ActionResult Subject()
         {
             int id = Convert.ToInt32(Request["CurrentCourseId"]);
@@ -24,6 +26,30 @@ namespace project.Controllers
             }
             ViewBag.subjects = subject;
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Subject(int subjecId)
+        {
+            //get new rate
+            int UserRate = Convert.ToInt32(Request["new_rating_star"]);
+            //cal and response: totalrate+1
+
+            //get value
+            var recordeSubjectId = (from s in db.Subjects where s.Id == subjecId select s).ToList();
+            Subject currentSubject = recordeSubjectId[0];
+
+            double newSubjectRate = Convert.ToDouble((currentSubject.Total_rate * currentSubject.Rate + UserRate) / (currentSubject.Total_rate + 1));
+
+            //update
+            var result = db.Subjects.SingleOrDefault(b => b.Id == currentSubject.Id);
+            if (result != null)
+            {
+                result.Rate = newSubjectRate;
+                result.Total_rate = result.Total_rate + 1;
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index","Home");
         }
     }
 }
